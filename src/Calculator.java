a8162b85fb92e472d980e87907f69e4705d841f6
MarioWintes <52406123@edu.campus02.at>
2024-09-26 21:44:20 +0200

    Method Caltulator

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,17 +5,47 @@ public class Calculator {
 
         int number1 = 0;
         int number2 = 0;
-        int result = 0;
+        int addition, subtraction, multiplication, division, rest = 0;
         String calcName = "MaschineBalthasar";
 
         Scanner scanner = new Scanner(System.in);
         number1 = scanner.nextInt();
         number2 = scanner.nextInt();
 
-        result = number1 + number2;
+        addition = add(number1, number2);
+        System.out.println("addition = " + addition);
 
-        System.out.println("Mein Name ist " + calcName + " " + result);
+        subtraction = subtract(number1, number2);
+        System.out.println("subtraction = " + subtraction);
+
+        multiplication = multiply(number1, number2);
+        System.out.println("multiplication = " + multiplication);
+
+        division = divide(number1, number2);
+        System.out.println("division = " + division);
+
+        rest = modulate(number1, number2);
+        System.out.println("modulo = " + rest);
 
 
+    }
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
+    public static int divide(int a, int b){
+        return a / b;
+    }
+
+    public static int modulate(int a, int b){
+        return a % b;
     }
 }
