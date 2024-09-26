7c3e36dc4ebb4488ecef02c0f7a7987d039c2ef0
MarioWintes <52406123@edu.campus02.at>
2024-09-26 17:09:21 +0200

    Ari finished

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -12,5 +12,23 @@ public class ArithmeticOperations {
         double divRest = (double) a /b;
         System.out.println("Division " + divRest);
         System.out.println("Modulo " + (a % b));
+
+
+        int j = 3;
+        System.out.println("j = " + j);
+        System.out.println("j = " + j++); // POST Increment erwartung 3
+        System.out.println("j = " + j); // hier ist j = 4
+        System.out.println("j = " + (++j));
+
+        int k = 3;
+        System.out.println("k = " + k);
+        System.out.println("k = " + (--k)); // PRE Increment erwartung 2
+        System.out.println("k = " + k);
+
+        String firstNameA = "Max";
+        String firstNameB = "Macx";
+
+        System.out.println(firstNameA == firstNameB); // Bei Strings wird nicht der Wert verglichen sondern die Referenz!!!
+        System.out.println(firstNameA.equals(firstNameB));
     }
 }
