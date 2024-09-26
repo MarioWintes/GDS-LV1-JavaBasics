0625edc43d46406d4127fabf1fdf9262a8cd18a3
MarioWintes <52406123@edu.campus02.at>
2024-09-26 15:24:20 +0200

    VariableDemo

---
+++ src/VariablesDemo.java
@@ -0,0 +1,32 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        // Deklaration
+        int a;
+
+        // Initialisieren
+        a = 100;
+        System.out.println(a);
+
+        long z = 200;
+        long y;
+
+        y = a;
+        //long to int nicht möglich (int b = y);
+
+        // Deklarieren
+        int c;
+        int e,f,g; // geht auch mehrfach
+        String name;
+
+        // Init
+        c = 2;
+        e = 3;
+        f = 4;
+        g = 5;
+        name = "Maxi Balthasar Mustermann";
+
+        //soutv + tab -> Automatische Ausgabe von Variablen
+        System.out.println("f = " + f);
+    }
+}
