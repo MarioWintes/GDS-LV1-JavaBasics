7cf65599b44964e1d80b5b761f0fb6443b83b04f
MarioWintes <52406123@edu.campus02.at>
2024-09-27 17:49:12 +0200

    floating point division

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 
         int number1 = 0;
         int number2 = 0;
-        int addition, subtraction, multiplication, division, rest = 0;
+        int addition, subtraction, multiplication, rest = 0;
         String calcName = "MaschineBalthasar";
 
         Scanner scanner = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class Calculator {
         multiplication = multiply(number1, number2);
         System.out.println("multiplication = " + multiplication);
 
-        division = divide(number1, number2);
+        double division = divide(number1, number2);
         System.out.println("division = " + division);
 
         rest = modulate(number1, number2);
@@ -29,6 +29,8 @@ public class Calculator {
 
 
     }
+    // access-modifier static return-value=(Datatype) methodName(parameters)
+    // public static void/int add(int x, int y)
     public static int add(int a, int b){
         return a + b;
     }
@@ -41,8 +43,14 @@ public class Calculator {
         return a * b;
     }
 
-    public static int divide(int a, int b){
-        return a / b;
+    public static double divide(int a, int b){
+        if (b != 0){
+            return a / (b * 1.0);
+            //return (double) a / b;
+        } else { // else eig nicht notwendig
+            System.out.println("Division by 0 not possible");
+            return 0;
+        }
     }
 
     public static int modulate(int a, int b){
